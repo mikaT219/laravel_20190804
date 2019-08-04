5ec7d1f54b768f1718f5380bb3d13eb994a2f402
Your Name <you@example.com>
2019-08-04 14:43:03 +0900

    laravelUpload

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    //
+    public function index() {
+  //    $posts = \App\Post::all();
+  //    $posts = Post::all();
+  //    $posts = Post::orderBy('created_at','desc');
+      $posts = Post::latest()->get();
+  //$posts = [];
+  //    dd($posts->toArray()); //dump die
+  //    return view('posts.index',['posts'=>$posts]);
+      return view('posts.index')->with('posts',$posts);
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Blog Posts</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Blog Posts</h1>
+    <ul>
+      {{--
+      @foreach ($posts as $post)
+      <li><a href="">{{ $post->title }}</a></li>
+      @endforeach
+      --}}
+      @forelse ($posts as $post)
+      <li><a href="">{{ $post->title }}</a></li>
+      @empty
+      <li>No posts yet</li>
+      @endforelse
+
+  </div>
+</body>
+</html>
