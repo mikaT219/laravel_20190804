1e039cdd447db7ddeb5f36b54360a151a0dc3466
Your Name <you@example.com>
2019-08-21 17:44:27 +0900

    firstcommit

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -18,4 +18,42 @@ class PostsController extends Controller
   //    return view('posts.index',['posts'=>$posts]);
       return view('posts.index')->with('posts',$posts);
     }
+
+    //public function show($id) {
+    public function show(Post $post) {
+      //$post = Post::findOrFail($id);
+      return view('posts.show')->with('post',$post);
+    }
+
+    public function create() {
+      return view('posts.create');
+    }
+
+    public function store(Request $request) {
+      $this->validate($request, [
+        'title' => 'required|min:3',
+        'body' => 'required'
+      ]);
+      $post = new Post();
+      $post->title = $request->title;
+      $post->body = $request->body;
+      $post->save();
+      return redirect('/');
+    }
+
+    public function edit(Post $post) {
+      return view('posts.edit')->with('post',$post);
+    }
+
+    public function update(Request $request, Post $post) {
+      $this->validate($request, [
+        'title' => 'required|min:3',
+        'body' => 'required'
+      ]);
+      $post->title = $request->title;
+      $post->body = $request->body;
+      $post->save();
+      return redirect('/');
+    }
+
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,24 +1,25 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>Blog Posts</title>
-</head>
-<body>
-  <div class="container">
-    <h1>Blog Posts</h1>
-    <ul>
-      {{--
-      @foreach ($posts as $post)
-      <li><a href="">{{ $post->title }}</a></li>
-      @endforeach
-      --}}
-      @forelse ($posts as $post)
-      <li><a href="">{{ $post->title }}</a></li>
-      @empty
-      <li>No posts yet</li>
-      @endforelse
+@extends('layouts.default')
 
-  </div>
-</body>
-</html>
+{{--
+@section('title')
+@endsection
+--}}
+
+@section('title','Blog Posts')
+
+@section('content')
+<h1>
+  <a href="{{ url('/posts/create')}}" class="header-menu">New Post</a>
+  Blog Posts
+</h1>
+<ul>
+  @forelse ($posts as $post)
+  <li>
+    <a href="{{ action('PostsController@show', $post ->id) }}">{{ $post->title }}</a>
+    <a href="{{ action('PostsController@edit', $post ->id) }}" class = "edit">[Edit]</a>
+  </li>
+  @empty
+  <li>No posts yet</li>
+  @endforelse
+
+@endsection
