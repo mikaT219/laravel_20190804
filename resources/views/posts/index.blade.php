f6f2a6125e7c4c5fae1f7aafeeeaed724cee8521
Your Name <you@example.com>
2019-08-25 12:43:49 +0900

    post_delete

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,9 +17,16 @@
   <li>
     <a href="{{ action('PostsController@show', $post ->id) }}">{{ $post->title }}</a>
     <a href="{{ action('PostsController@edit', $post ->id) }}" class = "edit">[Edit]</a>
+    <a href="#" class = "del" data-id="{{ $post->id }}">[x]</a>
+    <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{
+      $post->id }}">
+      {{ csrf_field() }}
+      {{ method_field('delete') }}
+    </form>
   </li>
   @empty
   <li>No posts yet</li>
   @endforelse
-
+</ul>
+<script src="/js/main.js"></script>
 @endsection
